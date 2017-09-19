25d9741c77838b8fa733500d0b2db8e3206e7a86
ebracho <eddieb@liatrio.com>
2017-09-18 22:34:50 -0700

    Content-Length header, threading

--- httpd.py
+++ httpd.py
@@ -2,45 +2,74 @@
 
 import socket
 import os
-from collections import namedtuple
+from concurrent.futures import ThreadPoolExecutor
 
-# namedtuples for now, will eventually move to classes
-HttpRequest = namedtuple('HttpRequest', ['method', 'path', 'protocol', 'headers'])
-HttpResponse = namedtuple('HttpResponse', ['protocol', 'code', 'msg', 'body'])
+class Request:
+	def __init__(self, conn, addr):
+		self.conn = conn
+		self.addr = addr
+		self.data = conn.recv(4096).decode('utf-8')
+		self.method, self.path, self.protocol, self.headers, self.body = self._parse_data()
 
-def parse_http_req(data):
-	lines = [l.strip() for l in data.strip().split('\n')]
-	if len(lines) == 0:
-		return HttpRequest('', '', '', '') # Malformed request, will handled later
-	status_line, headers = lines[0], lines[1:]
-	headers = dict(h.split(': ') for h in headers)
-	return HttpRequest(*status_line.split(), headers)
+	# TODO: Better parsing
+	def _parse_data(self):
+		headers, body = self.data.split('\r\n\r\n')
+		lines = [l.strip() for l in headers.strip().split('\n')]
+		status_line, headers = lines[0], lines[1:]
+		method, path, protocol = status_line.split()
+		headers = dict(h.split(': ') for h in headers)
+		return method, path, protocol, headers, body
 
-def build_response(req):
-	if not all([req.method, req.path, req.protocol]):
-		return HttpResponse(req.protocol, 400, 'Bad request', '')
-	filepath = os.path.realpath(os.path.join(os.getcwd(), req.path[1:] or 'index.html'))
-	if not filepath.startswith(os.getcwd()):
-		return HttpResponse(req.protocol, 403, 'Forbidden', '')
-	elif not os.path.isfile(filepath):
-		return HttpResponse(req.protocol, 404, 'Not Found', '')
-	else:
-		with open(filepath, 'r') as f:
-			return HttpResponse(req.protocol, 200, 'OK', f.read())
+	def __str__(self):
+		return f'[{self.addr[0]}] {self.method} {self.path} {self.protocol}'
+
+
+class Response:
+	def __init__(self, req):
+		self.req = req
+		self.protocol, self.code, self.msg, self.headers, self.body = self._build()
+
+	def _build(self):
+		if not all([self.req.method, self.req.path, self.req.protocol]):
+			return ('HTTP/1.1', 400, 'Bad request', {}, b'')
+		fp = os.path.realpath(os.path.join(os.getcwd(), self.req.path[1:] or 'index.html'))
+		if not fp.startswith(os.getcwd()):
+			return (self.req.protocol, 403, 'Forbidden',  {}, b'')
+		elif not os.path.isfile(fp):
+			return (self.req.protocol, 404, 'Not Found', {}, b'')
+		else:
+			with open(fp) as f:
+				body = f.read().encode('utf-8')
+				headers = {
+					'Content-Length': len(body),
+					'Content-Type': 'text/html; charset=utf-8'
+				}
+				return (self.req.protocol, 200, 'OK', headers, body)
+
+	def send(self):
+		with self.req.conn:
+			status_line = f'{self.protocol} {self.code} {self.msg}'
+			headers = [f'{k}: {v}' for k,v in self.headers.items()]
+			payload = ('\r\n'.join([status_line] + headers) + '\n\n').encode('utf-8') + self.body
+			self.req.conn.sendall(payload)
+
+	def __str__(self):
+		return f'{self.protocol} {self.code} {self.msg}'
+
 
 def handle_conn(conn, addr):
-	with conn:
-		data = conn.recv(4096) # TODO: handle longer requests
-		req = parse_http_req(data.decode('utf-8'))
-		print(f'[{addr[0]}] {req.method} {req.path} {req.protocol}')
-		res = build_response(req)
-		conn.sendall(f'{res.protocol} {res.code} {res.msg}\n\n{res.body}'.encode('utf-8'))
+	req = Request(conn, addr)
+	res = Response(req)
+	res.send()
+
 
 HOST = ''
 PORT = 3333
+MAX_THREADS = 1024
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind((HOST, PORT))
-	s.listen(1)
-	while True:
-		handle_conn(*s.accept())
-
+	s.listen(MAX_THREADS)
+	with ThreadPoolExecutor(MAX_THREADS) as ex:
+		while True:
+			ex.submit(handle_conn, *s.accept())
